ce71731c87aae77829f289dfa55b37fc699c83f9
Duvvuri Prathyusha <duvvuriprathyusha@duvvuris-air.home>
2020-11-13 20:01:50 -0500

    initial commit until select list from array

---
+++ src/app/employees/create-employee.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model'
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  departments: Department[] = [
+    { id: 1, name: 'Helpdesk'},
+    { id: 2, name: 'HR'},
+    { id: 3, name: 'IT'},
+    { id: 4, name: 'Payroll'}
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  saveEmployee(employeeForm: NgForm) : void {
+    console.log(employeeForm.value); 
+  }
+}
---
+++ src/app/employees/list-employees.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {Employee} from '../models/employee.model'
+
+@Component({
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'email',
+      email: 'asdf@sf.com',
+      dateOfBirth: new Date('11/2/20'),
+      department: 'IT',
+      isActive: true,
+      photoPath: 'assets/images/john.png'
+    },
+    {
+      id: 2,
+      name: 'Joe',
+      gender: 'FeMale',
+      contactPreference: 'email',
+      email: 'Joe@sf.com',
+      dateOfBirth: new Date('11/3/20'),
+      department: 'Mech',
+      isActive: true,
+      photoPath: 'assets/images/mark.png'
+    },
+    {
+      id: 1,
+      name: 'France',
+      gender: 'Male',
+      contactPreference: 'email',
+      email: 'France@sf.com',
+      dateOfBirth: new Date('11/4/20'),
+      department: 'Civil',
+      isActive: true,
+      photoPath: 'assets/images/mary.png'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
