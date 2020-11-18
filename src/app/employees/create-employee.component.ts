5d0296f7dec928ffd959d847615300b0af02fd8b
Duvvuri Prathyusha <duvvuriprathyusha@Duvvuris-MacBook-Air.local>
2020-11-18 16:18:43 -0500

    model binding

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model'
+import { Employee } from '../models/employee.model'
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
@@ -11,6 +12,18 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 export class CreateEmployeeComponent implements OnInit {
   previewPhoto: false;
   bsConfig: Partial<BsDatepickerConfig>;
+  employee : Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    email: null,
+    phoneNumber : null,
+    contactPreference: null,
+    dateOfBirth: null,
+    department: null,
+    isActive: null,
+    photoPath: null,
+  }
   departments: Department[] = [
     { id: 1, name: 'Helpdesk'},
     { id: 2, name: 'HR'},
@@ -25,8 +38,8 @@ export class CreateEmployeeComponent implements OnInit {
     
   }
 
-  saveEmployee(employeeForm: NgForm) : void {
-    console.log(employeeForm.value); 
+  saveEmployee(newEmployee: Employee) : void {
+    console.log(newEmployee); 
   }
 
   togglePhotoPreview(){
