1f1953e25b62318c9636fb1bebbe03898b1c8110
Duvvuri Prathyusha <duvvuriprathyusha@duvvuris-air.home>
2020-11-23 11:45:39 -0500

    Angular input change detection using property setter

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -6,20 +6,30 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges{
-  @Input() employee: Employee;
+export class DisplayEmployeeComponent implements OnInit{
+  private _employee: Employee;
+  @Input() 
+  set employee(val :Employee){
+    console.log('Previous Employee:' +(this._employee ? this._employee.name: 'NULL'));
+    console.log('Current: ' + val.name);
+    this._employee = val;
+  };
+
+  get employee(): Employee{
+    return this._employee;
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
-  ngOnChanges(changes: SimpleChanges){
-    console.log(changes);
-    const previousEmployee = <Employee>changes.employee.previousValue;
-    const currentEmployee = <Employee>changes.employee.currentValue;
+  // ngOnChanges(changes: SimpleChanges){
+  //   console.log(changes);
+  //   const previousEmployee = <Employee>changes.employee.previousValue;
+  //   const currentEmployee = <Employee>changes.employee.currentValue;
 
-    console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
-    console.log('Current' + currentEmployee.name);
-  }
+  //   console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
+  //   console.log('Current' + currentEmployee.name);
+  // }
 
 }
