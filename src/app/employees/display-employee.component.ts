d27d98ba6395dabb72b9e170e487e6c042b7895a
Duvvuri Prathyusha <duvvuriprathyusha@duvvuris-air.home>
2020-11-23 15:35:53 -0500

    parent to child communication @output property

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit , OnChanges , SimpleChanges, SimpleChange } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -7,45 +7,18 @@ import { Employee } from '../models/employee.model';
   styleUrls: ['./display-employee.component.css']
 })
 export class DisplayEmployeeComponent implements OnInit{
-  private _employeeId: number;
 
-  private _employee: Employee;
-  
-  @Input() 
-  set employee(val :Employee){
-    console.log('Previous Employee:' +(this._employee ? this._employee.name: 'NULL'));
-    console.log('Current: ' + val.name);
-    this._employee = val;
-  };
+  @Input() employee: Employee;
+  @Output() notify: EventEmitter<Employee> = new EventEmitter<Employee>();
 
-  @Input()
-  set employeeId(val : number){
-    console.log('employeeId changed from:' 
-    +JSON.stringify(this._employeeId) + 'to' + JSON.stringify(val));
-    this._employeeId = val;
-  }
 
-  get employeeId(): number{
-    return this._employeeId;
-  }
-
-  get employee(): Employee{
-    return this._employee;
-  }
   constructor() { }
 
   ngOnInit() {
   }
 
-  // ngOnChanges(changes: SimpleChanges){
-  //   for (const propName of Object.keys(changes)){
-  //     const change = changes[propName];
-  //     const from = JSON.stringify(change.previousValue);
-  //     const to = JSON.stringify(change.currentValue);
-
-  //     console.log(propName + 'changed from ' + from + 'to' + to);
-
-  //   }
-  // }
+  handleClick(){
+    this.notify.emit(this.employee);
+  }
 
 }
